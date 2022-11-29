b87b0a0eb25b95d9e02857a3654ad36926e041cf
Fre <freberhane@microsoft.com>
2022-11-29 14:53:29 +0300

    react keyboard navigation

--- fragments/HelloWorld.js
+++ fragments/HelloWorld.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Button,TextInput } from "react-native";
 import Title from "../components/Title";
 
 export default function HelloWorld() {
   return (
     <View style={styles.container}>
       <Title title="Hello World" />
+      <View style={styles.view_container}>
+        <Button title="RN Test"/>
+        <TextInput styles={styles.input}/>
+      </View>
     </View>
   );
 }
@@ -14,8 +18,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    backgroundColor: "#20232a",
+    backgroundColor: "transparent",
   },
+  view_container:{
+    margin: 20
+  },
+  input:{
+    marginTop: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    backgroundColor: "#fff"
+  }
 });
 
 /*
